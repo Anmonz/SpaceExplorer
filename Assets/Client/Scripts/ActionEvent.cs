c518d8f6ff9dafca0580b767b32bf9c43c6f567b
Andry <kramcheninov.andrey4@gmail.com>
2020-09-01 17:56:33 +0300

    Added support script ActionEvent

---
+++ Assets/Client/Scripts/ActionEvent.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using UnityEngine.Events;
+
+namespace com.AndryKram.SpaceExplorer
+{
+    public class ActionEvent
+    {
+        private event Action OnEventAction;
+
+        public void AddListener(Action action)
+        {
+            OnEventAction += action;
+        }
+
+        public void RemoveListener(Action action)
+        {
+            OnEventAction -= action;
+        }
+
+        public void RemoveAllListener()
+        {
+            OnEventAction = null;
+        }
+
+        public void Invoke()
+        {
+            OnEventAction?.Invoke();
+        }
+    }
+
+    public class ActionEvent<T0>
+    {
+        private event Action<T0> OnEventAction;
+
+        public void AddListener(Action<T0> action)
+        {
+            OnEventAction += action;
+        }
+
+        public void RemoveListener(Action<T0> action)
+        {
+            OnEventAction -= action;
+        }
+
+        public void RemoveAllListener()
+        {
+            OnEventAction = null;
+        }
+
+        public void Invoke(T0 message)
+        {
+            OnEventAction?.Invoke(message);
+        }
+    }
+
+    public class ActionEvent<T0,T1>
+    {
+        private event Action<T0,T1> OnEventAction;
+
+        public void AddListener(Action<T0,T1> action)
+        {
+            OnEventAction += action;
+        }
+
+        public void RemoveListener(Action<T0,T1> action)
+        {
+            OnEventAction -= action;
+        }
+
+        public void RemoveAllListener()
+        {
+            OnEventAction = null;
+        }
+
+        public void Invoke(T0 message1, T1 message2)
+        {
+            OnEventAction?.Invoke(message1,message2);
+        }
+    }
+
+    public class ActionEvent<T0, T1, T2>
+    {
+        private event Action<T0, T1, T2> OnEventAction;
+
+        public void AddListener(Action<T0, T1, T2> action)
+        {
+            OnEventAction += action;
+        }
+
+        public void RemoveListener(Action<T0, T1, T2> action)
+        {
+            OnEventAction -= action;
+        }
+
+        public void RemoveAllListener()
+        {
+            OnEventAction = null;
+        }
+
+        public void Invoke(T0 message1, T1 message2, T2 message3)
+        {
+            OnEventAction?.Invoke(message1, message2, message3);
+        }
+    }
+
+    public class ActionEvent<T0, T1, T2, T3>
+    {
+        private event Action<T0, T1, T2, T3> OnEventAction;
+
+        public void AddListener(Action<T0, T1, T2, T3> action)
+        {
+            OnEventAction += action;
+        }
+
+        public void RemoveListener(Action<T0, T1, T2, T3> action)
+        {
+            OnEventAction -= action;
+        }
+
+        public void RemoveAllListener()
+        {
+            OnEventAction = null;
+        }
+
+        public void Invoke(T0 message1, T1 message2, T2 message3, T3 message4)
+        {
+            OnEventAction?.Invoke(message1, message2, message3, message4);
+        }
+    }
+}
